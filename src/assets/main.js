3102798db32f6a6c066439dffbf88551453e8c51
Gedokawa <grzsum@wp.pl>
2017-03-04 15:41:18 +0100

    I added respone.courses.completed and added div element

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,20 @@ $(function() {
   $.ajax({
     url: 'https://www.codeschool.com/users/1174931.json',
     dataType: 'jsonp',
-    success: function(resposne){
-
+    success: function(response){
+        addCourses(response.courses.completed);
     }
   });
+
+  function addCourses(courses){
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course){
+
+      $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+    })
+  }
 });
