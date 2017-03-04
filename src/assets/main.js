a268fab256a42cbceb2565a5c1898ed48f57cd53
Gedokawa <grzsum@wp.pl>
2017-03-04 15:25:33 +0100

    I added my report card url

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/1174931.json',
+    dataType: 'jsonp',
+    success: function(resposne){
 
+    }
+  });
 });
