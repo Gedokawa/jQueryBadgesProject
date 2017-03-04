9453c3ea456c0ecf45bb55f19281ffe2832d0060
Gedokawa <grzsum@wp.pl>
2017-03-04 15:48:43 +0100

    addes h3, img, a

--- src/assets/main.js
+++ src/assets/main.js
@@ -11,13 +11,31 @@ $(function() {
 
   function addCourses(courses){
 
+
+
     var $badges = $('#badges');
 
     courses.forEach(function(course){
 
-      $('<div />', {
+      console.log(course);
+
+      var $course = $('<div />', {
         'class': 'course'
       }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        target: '_blank',
+        href: 'course.url',
+        text: 'See course'
+      }).appendTo($course);
     })
   }
 });
